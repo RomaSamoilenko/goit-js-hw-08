db80bd3b8e3c9a7b9117c8fef37e33d1d737e76e
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-02-02 18:29:57 +0200

    1

---
+++ js/gallery.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+function createGalleryItemsMarkup(items) {
+    return items
+      .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+      })
+      .join('');
+  } 
