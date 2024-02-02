b57943a00b4461296c7c260efa6b4cbe48ec8df8
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-02-02 21:56:21 +0200

    1

--- js/gallery.js
+++ js/gallery.js
@@ -66,10 +66,10 @@ const images = [
 
     
 
-    const container = document.querySelector("ul.images");
-    container.innerHTML = createImagesMarkup(images);
+    const ulGallery = document.querySelector(".gallery");
+    ulGallery.innerHTML = createGalleryMarkup(images);
     
-    function createImagesMarkup(images) {
+    function createGalleryMarkup(images) {
       return images
         .map(({ original, preview, description }) => {
           return `<li class="gallery-item">
@@ -86,16 +86,16 @@ const images = [
         .join('');
     }
     
-    console.log(createImagesMarkup(images));
+    
     
     // galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
     // galleryContainer.addEventListener('click', selectGalleryEl);
     
-    // function selectGalleryEl(event) {
-    //   event.preventDefault();
-    //   if (event.target.nodeName !== 'IMG') {
-    //     return;
-    //   }
+    function selectGalleryEl(event) {
+      event.preventDefault();
+      if (event.target.nodeName !== 'IMG') 
+        return;
+      }
     //   const instance = basicLightbox.create(
     //     `<img src="${event.target.dataset.source}" width="800" height="600" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     //     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">`,
